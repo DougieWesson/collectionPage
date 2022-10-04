1afb997d9626c84d1994d0b059f023f2520c8ae9
EyUp Student <eyupstudent@EyUps-MacBook-Air.local>
2022-10-04 13:58:47 +0100

    refactored code into better util form

--- collectionPageUtil.js
+++ collectionPageUtil.js
@@ -44,7 +44,6 @@ const isPictureInputNull = () => {
     return document.forms["inputForm"]["cardPic"].value == "";
 }
 
-
 const areAllEditsNull = () => {
     if (
         document.forms["editForm"]["cardName"].value == ""
@@ -89,4 +88,188 @@ const isCardValueEditANumber = () => {
 
 const isPictureEditNull = () => {
     return document.forms["editForm"]["cardPic"].value == "";
+}
+
+const toggleRemoveMode = () => {
+    removeMode = !removeMode;
+        let deleteButtons = document.getElementsByClassName('removeButton')
+    for (let i=0; i<deleteButtons.length; i++) {
+        deleteButtons[i].disabled = !removeMode;
+    };
+}
+
+const empowerRemoveButtons = () => {
+    cardList.forEach((x, i) => {
+        document.getElementById('removeId' + i).onclick = () => {
+            cardList.splice(i, 1);
+            listMyCards();
+            localStorage.setItem('cardList', JSON.stringify(cardList));
+        };
+        });
+}
+
+const makeRemoveButtonsConsistent = () => {
+    let deleteButtons = document.getElementsByClassName('removeButton')
+        if (removeMode == false) {
+            for (let i=0; i<deleteButtons.length; i++) {
+            deleteButtons[i].disabled = !removeMode;
+            };
+        }
+}
+
+const attachPictureToCard = () => {
+    cardList.forEach((x, i) => {
+        const preview = document.getElementById('pictureId' + i);
+        const image = new Image();
+        image.height = 180;
+        image.width = 120;
+        image.title = x[cardName];
+        image.src = x[cardPic];
+        preview.appendChild(image);
+        });
+}
+
+const listMyCards = () => {
+    let listOfCards = '';
+    cardList.forEach((x, i) => {
+        listOfCards += `<li> Card Name: ${x[cardName]}
+                        <br> Card Type: ${x[cardType]}
+                        <br> Mana Cost: ${x[manaCost]}
+                        <br> Card Value: ${x[cardValue]}
+                        <br><div id=${"pictureId" + i}></div>
+                        <br> <input type="button" value="Remove" class="removeButton" id=${"removeId" + i}>
+                        <br>
+                        </li>`
+    });
+    document.getElementById('card-list').innerHTML = listOfCards;
+    localStorage.setItem('cardList', JSON.stringify(cardList));
+
+    empowerRemoveButtons();
+    attachPictureToCard();
+    makeRemoveButtonsConsistent();
+}
+
+const sortMyCards = () => {
+    if(sortTypeSelector.value == "Ascending") {
+    switch(sortSelector.value) {
+        case "Card Name":
+            cardList.sort((a,b) => {
+                if(a.cardName > b.cardName) {
+                    return 1;
+                } else if (a.cardName < b.cardName) {
+                    return -1;
+                } else {
+                    return 0;
+                };
+            });
+            break;
+        case "Card Type":
+            cardList.sort((a,b) => {
+                if(a.cardType > b.cardType) {
+                    return 1;
+                } else if (a.cardType < b.cardType) {
+                    return -1;
+                } else {
+                    return 0;
+                };
+            });
+            break;
+        case "Mana Cost":
+            cardList.sort((a,b) => {
+                if(a.manaCost > b.manaCost) {
+                    return 1;
+                } else if (a.manaCost < b.manaCost) {
+                    return -1;
+                } else {
+                    return 0;
+                };
+            });
+            break;
+        case "Card Value":
+            cardList.sort((a,b) => {
+                if(a.cardValue > b.cardValue) {
+                    return 1;
+                } else if (a.cardValue < b.cardValue) {
+                    return -1;
+                } else {
+                    return 0;
+                };
+            });
+            break;
+    }
+} else if (sortTypeSelector.value == "Descending") {
+    switch(sortSelector.value) {
+        case "Card Name":
+            cardList.sort((a,b) => {
+                if(a.cardName > b.cardName) {
+                    return -1;
+                } else if (a.cardName < b.cardName) {
+                    return 1;
+                } else {
+                    return 0;
+                };
+            });
+            break;
+        case "Card Type":
+            cardList.sort((a,b) => {
+                if(a.cardType > b.cardType) {
+                    return -1;
+                } else if (a.cardType < b.cardType) {
+                    return 1;
+                } else {
+                    return 0;
+                };
+            });
+            break;
+        case "Mana Cost":
+            cardList.sort((a,b) => {
+                if(a.manaCost > b.manaCost) {
+                    return -1;
+                } else if (a.manaCost < b.manaCost) {
+                    return 1;
+                } else {
+                    return 0;
+                };
+            });
+            break;
+        case "Card Value":
+            cardList.sort((a,b) => {
+                if(a.cardValue > b.cardValue) {
+                    return -1;
+                } else if (a.cardValue < b.cardValue) {
+                    return 1;
+                } else {
+                    return 0;
+                };
+            });
+            break;
+    }
+}
+}
+
+const addCard = () => {
+    const card = {
+        [cardName]:document.forms.inputForm.cardNameInput.value,
+        [cardType]:document.forms.inputForm.cardTypeInput.value,
+        [manaCost]:document.forms.inputForm.manaCostInput.value,
+        [cardPic]:dataURL,
+        [cardValue]:document.forms.inputForm.cardValueInput.value,
+    }
+    cardList.push(card);
+    dataURL = '';
+}
+
+const editCard = () => {
+    const card = {
+        [cardName]:document.forms.editForm.cardNameEdit.value,
+        [cardType]:document.forms.editForm.cardTypeEdit.value,
+        [manaCost]:document.forms.editForm.manaCostEdit.value,
+        [cardPic]:dataURL,
+        [cardValue]:document.forms.editForm.cardValueEdit.value,
+    }
+    const filterKey = document.forms["editForm"]["cardName"].value;
+    let originalIndex = cardList.map(card => card.cardName).indexOf(document.forms["editForm"]["cardName"].value);
+    cardList.splice(originalIndex, 1)
+    cardList.push(card);
+    dataURL = '';
 }
